a516986c54b093e00e91beba2c7d644d5e59b077
sisterai <49988600+sisterai@users.noreply.github.com>
2025-04-21 18:09:00 -0400

    Fix: Added MoE & Visualizations to App

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -80,7 +80,7 @@ const sliderSettings = {
   arrows: true,
   infinite: false,
   autoplay: true,
-  autoplaySpeed: 2500,
+  autoplaySpeed: 3000,
   speed: 500,
   slidesToShow: 3,
   slidesToScroll: 1,
